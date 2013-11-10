1ac039860023357896b401dac5318b3a769562a9
David Whitney <david@davidwhitney.co.uk>
2013-11-10 17:05:20 +0000

    tests for parser

---
+++ UTorrentPostDownloadScript.Test.Unit/Features/ArgumentParsing/ParsableArgumentsTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using UTorrentPostDownloadScript.Features.ArgumentParsing;
+
+namespace UTorrentPostDownloadScript.Test.Unit.Features.ArgumentParsing
+{
+    [TestFixture]
+    public class ParsableArgumentsTests
+    {
+        [Test]
+        public void WhenParserMappingAdded_AndValueOnQueryString_ValueMappedAccordingToParser()
+        {
+            var parser = new ParsableArguments<SomeClass> {{"a", (@class, s) => @class.SomeProperty = s}};
+
+            var parsed = parser.Parse(new[] {"-a", "value"});
+
+            Assert.That(parsed.SomeProperty, Is.EqualTo("value"));
+        }
+
+        public class SomeClass
+        {
+            public string SomeProperty { get; set; }
+        }
+    }
+}
