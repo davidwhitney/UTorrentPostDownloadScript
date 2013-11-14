76740ec244f5285c711986c479e27503deb29ac1
David Whitney <david@davidwhitney.co.uk>
2013-11-14 22:14:38 +0000

    added global error handling

--- UTorrentPostDownloadScript/Program.cs
+++ UTorrentPostDownloadScript/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using log4net;
 using log4net.Config;
 using Ninject;
 
@@ -9,8 +10,16 @@ namespace UTorrentPostDownloadScript
         public static void Main(string[] args)
         {
             XmlConfigurator.Configure();
-            var kernel = new StandardKernel(new Bindings());
-            kernel.Get<TorrentDownloadedAction>().Execute(args);
+
+            try
+            {
+                var kernel = new StandardKernel(new Bindings());
+                kernel.Get<TorrentDownloadedAction>().Execute(args);
+            }
+            catch (Exception ex)
+            {
+                LogManager.GetLogger("Log").Error(ex);
+            }
         }
     }
 }
